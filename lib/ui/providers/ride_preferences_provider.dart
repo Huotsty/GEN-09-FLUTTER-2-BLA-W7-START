6348662733a5b8e87c76f9a9552711787f95644d
Huotsty <122008167+Huotsty@users.noreply.github.com>
2025-03-21 20:37:25 +0700

    STEP 3  -   Update RidesPreferencesProvider

--- lib/ui/providers/ride_preferences_provider.dart
+++ lib/ui/providers/ride_preferences_provider.dart
@@ -2,11 +2,15 @@ import 'package:flutter/material.dart';
 import 'package:week_3_blabla_project/model/ride/ride_pref.dart';
 import 'package:week_3_blabla_project/repository/ride_preferences_repository.dart';
 
+import 'async_value.dart';
+
 class RidesPreferencesProvider extends ChangeNotifier {
   RidePreference? _currentPreference;
   List<RidePreference> _pastPreferences = [];
 
   final RidePreferencesRepository repository;
+
+  late AsyncValue<List<RidePreference>> pastPreferences;
 
   RidesPreferencesProvider({required this.repository}) {
     // For now past preferences are fetched only 1 time
@@ -29,30 +33,52 @@ class RidesPreferencesProvider extends ChangeNotifier {
     }
   }
 
-  void _addPreference(RidePreference preference) {
+  Future<void> _addPreference(RidePreference preference) async {
     // if (!_pastPreferences.contains(preference)) {
     //   _pastPreferences.add(preference);
     //   repository.addPreference(preference);
     //   notifyListeners();
     // }
-    bool isDuplicate = _pastPreferences.any((p) => p == preference);
+    await repository.addPreference(preference);
+    // ✅ Ensure no duplicates before adding to cache
+    // if (!_pastPreferences.contains(preference)) {
+    //   _pastPreferences.add(preference);
+    //   notifyListeners();
+    // Remove the preference if it already exists
+    _pastPreferences.remove(preference);
 
-    if (!isDuplicate) {
-      _pastPreferences.add(preference);
-      repository.addPreference(preference);
-      notifyListeners();
-    }
+    // Add the preference to the front of the list
+    _pastPreferences.insert(0, preference);
+
+    notifyListeners();
   }
 
   // History is returned from newest to oldest preference
-  List<RidePreference> get preferencesHistory =>
-      _pastPreferences.reversed.toList();
+  List<RidePreference> get preferencesHistory => _pastPreferences.toList();
 
-  void fetchPastPreferences() {
-    _pastPreferences = repository.getPastPreferences();
-
-    // ✅ Remove duplicates based on departure & arrival
-    _pastPreferences = _pastPreferences.toSet().toList();
+  Future<void> fetchPastPreferences() async {
+    // 1-  Handle loading
+    pastPreferences = AsyncValue.loading();
     notifyListeners();
+    try {
+      // 2   Fetch data
+      _pastPreferences = await repository.getPastPreferences();
+      // ✅ Remove duplicates based on departure & arrival
+      _pastPreferences = _pastPreferences.toSet().toList();
+      // 3  Handle success
+      pastPreferences = AsyncValue.success(_pastPreferences);
+      // 4  Handle error
+    } catch (error) {
+      pastPreferences = AsyncValue.error(error);
+    }
+    notifyListeners();
+    // _pastPreferences = await repository.getPastPreferences();
+
+    // // ✅ Remove duplicates based on departure & arrival
+    // _pastPreferences = _pastPreferences.toSet().toList();
+    // notifyListeners();
   }
 }
+
+
+// choose the second bcuz we give the database to add the past preferences and we set to the provider the past preferences no need to fetch the data again but we have to check the duplicates before adding the new preference to the past preferences list
